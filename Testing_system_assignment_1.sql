f137a734b7c8ff2b9a6baec784ab0bd4fc5043ac
Hoang <87769312+soibetixiu@users.noreply.github.com>
2022-11-28 20:36:50 +0700

    Add files via upload

---
+++ Testing_system_assignment_1.sql
@@ -0,0 +1,60 @@
+create table hoang;
+use hoang;
+
+CREATE TABLE department (
+    departmentid INT,
+    departmentname VARCHAR(50)
+);
+CREATE TABLE position (
+    position_id INT,
+    position_name VARCHAR(50)
+);
+CREATE TABLE account_id (
+    QuestionID INT,
+    email VARCHAR(100),
+    username VARCHAR(100),
+    fullname VARCHAR(100),
+    department_id INT,
+    position_id INT,
+    creatdate DATE
+);
+CREATE TABLE groupH (
+    group_id INT,
+    group_name VARCHAR(50),
+    creator_id INT,
+    creatdate DATE
+);
+CREATE TABLE GroupAccount (
+    Group_id INT,
+    Account_id INT,
+    Joindate DATE
+);
+CREATE TABLE TypeQuestion (
+    Type_id INT,
+    TypeName VARCHAR(50)
+);
+CREATE TABLE CategoryQuestion (
+    CategoryID INT,
+    CategoryName VARBINARY(50)
+);
+CREATE TABLE Question (
+    QuestionID INT,
+    Content VARCHAR(50),
+    CategoryID INT,
+    TypeID INT,
+    CreatorID INT,
+    CreateDate DATE
+);
+CREATE TABLE Exam (
+    ExamID INT,
+    Codes INT,
+    Title VARCHAR(50),
+    CategoryID INT,
+    Duration TIME,
+    CreatorID INT,
+    CreateDate DATE
+);
+CREATE TABLE ExamQuestion (
+    ExamID INT,
+    QuestionID INT
+);
